b184d50752145aed6268110c5af3834d11ce600b
SarthakShah001 <108401951+SarthakShah001@users.noreply.github.com>
2023-01-08 20:01:44 +0530

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define int long long
+#define INTMAX 1000000007
+#define vi vector<int>
+#define si set<int>
+#define F first
+#define S second
+#define pb push_back
+#define vp vector<pair<int, int>>
+#define pair pair<int, int>
+using namespace std;
+#define MAX 100000
+
+void solve(){
+    
+}
+
+signed main()
+{
+    ios::sync_with_stdio(0);
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+ 
+        solve();
+        cout << endl;
+    }
+}
